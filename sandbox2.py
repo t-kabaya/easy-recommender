35169a1f61901629300766ed7e823afc609102b5
t-kabaya <taikikabaya@gmail.com>
2025-08-10 16:23:39 +0900

    Add recommendation functionality to sandbox2.py

--- sandbox2.py
+++ sandbox2.py
@@ -1,11 +1,15 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from typing import List, Tuple, Dict
+from easy_recommender.recommender import recommend
 
 def main():
     dataset_path = "datasets/ml-1m/"
     df = pd.read_csv(f"{dataset_path}movielens_combined.csv")
     print(df.head())
+
+    res = recommend(df, user_features, item_features)
+    print(res)
 
 if __name__ == "__main__":
     main()
